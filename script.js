92051b124be92b17af365b8e90e86e6804ef710a
Ostapchuk Eduard <ostapchuk95edyard@gmail.com>
2020-01-21 21:21:25 +0200

    js1-4

---
+++ script.js
@@ -0,0 +1,32 @@
+// work-home 1
+
+// let a = 2.4;
+//     b = 3.3;
+// let result = a * b;
+// let numResult = parseFloat(result.toFixed(1))
+// console.log(numResult, typeof numResult)
+
+// work-home 2
+
+// let name = 'Polina',
+//     age = 25,
+//     yearofbirth = 2020;
+
+// let MyBirth = yearofbirth - age;
+
+// console.log(`My name is ${name}. I was born in ${MyBirth} and I'm ${age} years old`);
+
+// option 2
+
+// let numYear = 2020;
+// let firstName = prompt('Как вас зовут?');
+// let years = prompt('Сколько вам лет?');
+// let resultBirth = numYear - years;
+// alert(firstName + ',Ваш год рождения' + resultBirth + '.')
+
+
+// work-home 3
+
+// let r = parseFloat('5.67cm');
+//     result = Math.floor(Math.PI * (Math.pow(r, 2)));
+//     console.log(`Circle square is ${result}`)
---
+++ script2.js
@@ -0,0 +1,37 @@
+
+
+// work-home 4
+// let UserNumber = confirm ('Сыграем?')
+// console.log(UserNumber, "type: ", typeof(UserNumber))
+
+// if (UserNumber) {
+//     let num = +prompt('Введите число')
+//     console.log('Inputed number: ', num, "type:", typeof num )
+//     if (isNaN(num)) {
+//         alert('Вы ввели буквы')
+//     } else {
+//         if (!num) {
+//             alert('Вы отказались')
+//         } else {
+//             if (num % 2 === 0) {
+//                 alert('Четное')
+//             } else {
+//                 if (num % 2 === 1) 
+//                 alert('Не четное')
+//             } 
+//         }
+//     }
+// } 
+
+
+// Work - home 5
+
+// let user = +prompt('Введите число'),
+//     result
+//     if (user<0) {
+//         result = user % 3 === 0? user ** 2 : user % 8;
+//     }
+//         else if(user<0) {
+//             let root = Math.ceil(Math.sqrt(user));
+//             result
+//         }
